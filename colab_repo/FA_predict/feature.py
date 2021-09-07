bc0b5fa2aa77dd6f686e0c2f450d88ca9c804939
VivianMonzon <vmonzon@ebi.ac.uk>
2021-09-07 10:17:54 +0100

    folder/files creation

---
+++ colab_repo/FA_predict/feature.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+
+
+def preparations(seq, analysisfolder, resultsfolder):
+    if os.path.exists('{}'.format(analysisfolder)):
+        print('{} folder exists'.format(analysisfolder))
+    else:
+        os.system('mkdir {}'.format(analysisfolder))
+        print('{} folder created'.format(analysisfolder))
+    if os.path.exists('{}'.format(resultsfolder)):
+        print('{} folder exists'.format(resultsfolder))
+    else:
+        os.system('mkdir {}'.format(resultsfolder))
+        print('{} folder created'.format(resultsfolder))
+    fh_seq = open('query_seq.fa', 'w')
+    fh_seq.write('>query_sequence\n{}\n'.format(seq))
+    fh_seq.close()
+
+
+class collect_features():
+    
+    def hmmsearch():
+        os.system('hmmsearch --cut_ga --domtblout analysis/query_seq_adh_dom.tbl data/adh_dom_hmms.hmm query_seq.fa > analysis/query_seq_adh_dom.out')
+        print('hmmsearch adh done')
+        # python3.7 lib/make_out_readable.py --domtblout analysis/query_seq_adh_dom.tbl --out_readable analysis/query_seq_adh_dom_readable.tbl --out_important analysis/query_seq_adh_dom_important.tbl
