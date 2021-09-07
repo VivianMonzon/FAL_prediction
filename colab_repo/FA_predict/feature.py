849b5b0d13962f9ac1959d2e4ca2cd2764dbf0f8
VivianMonzon <vmonzon@ebi.ac.uk>
2021-09-07 17:02:46 +0100

    ML prediction func

--- colab_repo/FA_predict/feature.py
+++ colab_repo/FA_predict/feature.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import re
 from math import log2
+from sklearn.ensemble import RandomForestClassifier
 
 
 def preparations(seq, analysisfolder, resultsfolder):
@@ -163,7 +164,7 @@ class collect_features():
 
     def number_stalk(hmm_stalk):
         df = pd.read_csv(hmm_stalk, sep='\t', names=hmmsearch_cols)
-        print(df)
+        # print(df)
         count_s = df['ID'].value_counts().rename_axis('ID').reset_index(
             name='counts')
         df_count_stalk = count_s.rename({'counts': 'Stalks'}, axis=1)
@@ -183,10 +184,27 @@ class collect_features():
         cellwall_df['cellwall'] = 1
         PSE_df = cellwall_df[['ID', 'cellwall']]
         return PSE_df
-    
-    def combine_feature():
-        pass
 
 
-# df_stalks = collect_features.number_stalk('tmp.tbl')
-# print(df_stalks)
+class ML_prediction():
+     
+    def run_random_forest(features, training_set,
+                          resultsfolder, output_fh):
+        proteins = pd.read_csv(training_set)
+        proteins = proteins.drop_duplicates()
+        proteins = proteins.drop('ID', axis=1)
+        proteins = proteins.dropna()
+        X_proteins = proteins.drop('Type', axis=1)
+        y_proteins = proteins['Type']
+        model = RandomForestClassifier(n_estimators=50,
+                                       random_state=2, max_features=3)
+        model.fit(X_proteins, y_proteins)
+        test_set = pd.read_csv(features)
+        test_set = test_set.dropna()
+        test_set_wo_id = test_set.drop('ID', axis=1)
+        y_model = model.predict(test_set_wo_id)
+        probability_1 = model.predict_proba(test_set_wo_id)[:, 1]
+        test_set['prediction'] = y_model
+        test_set['probability_Adh'] = probability_1
+        test_set.to_csv('{}/{}.tsv'.format(resultsfolder,
+                                           output_fh), index=False, sep='\t')
